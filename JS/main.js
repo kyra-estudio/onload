399feff50b7668c37a93d1658368e282a81c6d76
kyra-estudio <marta.bravo76@gmail.com>
2021-06-17 16:34:45 +0200

    onload end

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+function welcome(){ 
+    alert("Bienvenidos a la página de Noruega")
+    const element = document.getElementById("copy_head")
+    element.innerHTML = "Bienvenidos a la página de Noruega"
+} 
